a10d6c51ddc10e0e9d34ec79676b1042fe93fc2f
Ashish Papanai <ashishpapanai00@gmail.com>
2021-02-22 22:52:07 +0530

    Database and Login

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ia7n8a8p7a3p0h0s1i5h8s0a'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
